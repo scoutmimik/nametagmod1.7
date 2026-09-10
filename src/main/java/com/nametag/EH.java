7d3ec61f852664ad79008f2050d50964422bffa9
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-10 14:39:32 +0200

    Update EH.java

--- src/main/java/com/nametag/EH.java
+++ src/main/java/com/nametag/EH.java
@@ -1,12 +1,12 @@
-package fiw.nametageditor;
+package com.nametag;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.client.event.RenderLivingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EH {
    @SubscribeEvent
-   public void onLabelRender(RenderLivingEvent.Specials.Pre<EntityLivingBase> e) {
+   public void onLabelRender(RenderLivingEvent.Specials.Pre e) {
       e.setCanceled(true);
       NametagRenderer.a = e.renderer;
       NametagRenderer.renderName(e.entity, e.x, e.y, e.z);
