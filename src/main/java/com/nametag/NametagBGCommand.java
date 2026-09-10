6ecb7c33debf6270a678575311bbd45a39e150a2
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-10 14:39:59 +0200

    Update NametagBGCommand.java

--- src/main/java/com/nametag/NametagBGCommand.java
+++ src/main/java/com/nametag/NametagBGCommand.java
@@ -1,4 +1,4 @@
-package fiw.nametageditor;
+package com.nametag;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,47 +6,46 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.util.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class NametagBGCommand implements ICommand {
-   public String func_71517_b() {
+   public String getCommandName() {
       return "nametag";
    }
 
-   public String func_71518_a(ICommandSender sender) {
+   public String getCommandUsage(ICommandSender sender) {
       return "/nametag";
    }
 
-   public void func_71515_b(ICommandSender sender, String[] args) throws CommandException {
+   public void processCommand(ICommandSender sender, String[] args) throws CommandException {
       MinecraftForge.EVENT_BUS.register(this);
    }
 
    @SubscribeEvent
    public void onClientTick(TickEvent.ClientTickEvent event) {
       MinecraftForge.EVENT_BUS.unregister(this);
-      Minecraft.func_71410_x().func_147108_a(new GUIMain());
+      Minecraft.getMinecraft().displayGuiScreen(new GUIMain());
    }
 
-   public boolean func_71519_b(ICommandSender sender) {
+   public boolean canCommandSenderUseCommand(ICommandSender sender) {
       return true;
    }
 
-   public int compareTo(ICommand o) {
+   public int compareTo(Object o) {
       return 0;
    }
 
-   public boolean func_82358_a(String[] args, int index) {
+   public boolean isUsernameIndex(String[] args, int index) {
       return false;
    }
 
-   public List<String> func_71514_a() {
+   public List getCommandAliases() {
       return new ArrayList();
    }
 
-   public List<String> func_180525_a(ICommandSender sender, String[] args, BlockPos pos) {
+   public List addTabCompletionOptions(ICommandSender sender, String[] args) {
       return null;
    }
 }
