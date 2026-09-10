58f91dc898e2e6112a4b5765f7134fd4ced8ea14
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-10 18:55:41 +0200

    Update EH.java

--- src/main/java/com/nametag/EH.java
+++ src/main/java/com/nametag/EH.java
@@ -1,14 +1,21 @@
 package com.nametag;
 
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EH {
    @SubscribeEvent
    public void onLabelRender(RenderLivingEvent.Specials.Pre e) {
-      e.setCanceled(true);
-      NametagRenderer.a = e.renderer;
-      NametagRenderer.renderName(e.entity, e.x, e.y, e.z);
+      // Reagujeme IBA na reálnych hráčov
+      if (e.entity instanceof EntityPlayer) {
+         // Zrušíme iba originálny render hráča
+         e.setCanceled(true);
+         
+         NametagRenderer.a = e.renderer;
+         NametagRenderer.renderName(e.entity, e.x, e.y, e.z);
+      }
+      // Pre všetky ostatné entity (NPC, hologramy, mobov) event NEZRUŠÍME.
+      // Hra pre ne vykreslí ich vlastné nametagy/hologramy presne na mieste, kde majú byť.
    }
 }
