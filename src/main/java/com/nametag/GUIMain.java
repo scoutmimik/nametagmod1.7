7c58d1209a2492a261ffabedfe66a69a5af7a0bb
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-10 14:39:07 +0200

    Update GUIMain.java

--- src/main/java/com/nametag/GUIMain.java
+++ src/main/java/com/nametag/GUIMain.java
@@ -1,11 +1,11 @@
-package fiw.nametageditor;
+package com.nametag;
 
 import java.io.IOException;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.fml.client.config.GuiSlider;
+import cpw.mods.fml.client.config.GuiSlider;
 
 public class GUIMain extends GuiScreen {
    private GuiButton buttonSelfTag;
@@ -19,13 +19,13 @@ public class GUIMain extends GuiScreen {
    private Integer[] positionY;
    private int positionX;
 
-   public void func_73866_w_() {
+   public void initGui() {
       int buttonLength = 180;
       this.centerButtons(5, buttonLength);
-      this.field_146292_n.add(this.buttonSelfTag = new GuiButton(1, this.positionX, this.positionY[0], buttonLength, 20, "Own nametag: " + this.getSelfTag()));
-      this.field_146292_n.add(this.alpha = new GuiSlider(7, this.positionX, this.positionY[1], buttonLength, 20, "Background alpha: ", "", (double)0.0F, (double)25.0F, (double)(NametagRenderer.alpha * 100.0F), false, true));
-      this.field_146292_n.add(this.sliderOffset = new GuiSlider(2, this.positionX, this.positionY[3], buttonLength, 20, "Y Offset: ", "", (double)-20.0F, (double)0.0F, (double)NametagRenderer.getOffset(), false, true));
-      this.field_146292_n.add(this.sliderScale = new GuiSlider(3, this.positionX, this.positionY[4], buttonLength, 20, "Scale: ", "%", (double)0.0F, (double)100.0F, (double)NametagRenderer.getScale(), false, true));
+      this.buttonList.add(this.buttonSelfTag = new GuiButton(1, this.positionX, this.positionY[0], buttonLength, 20, "Own nametag: " + this.getSelfTag()));
+      this.buttonList.add(this.alpha = new GuiSlider(7, this.positionX, this.positionY[1], buttonLength, 20, "Background alpha: ", "", (double)0.0F, (double)25.0F, (double)(NametagRenderer.alpha * 100.0F), false, true));
+      this.buttonList.add(this.sliderOffset = new GuiSlider(2, this.positionX, this.positionY[3], buttonLength, 20, "Y Offset: ", "", (double)-20.0F, (double)0.0F, (double)NametagRenderer.getOffset(), false, true));
+      this.buttonList.add(this.sliderScale = new GuiSlider(3, this.positionX, this.positionY[4], buttonLength, 20, "Scale: ", "%", (double)0.0F, (double)100.0F, (double)NametagRenderer.getScale(), false, true));
    }
 
    private String getSelfTag() {
@@ -33,21 +33,20 @@ public class GUIMain extends GuiScreen {
    }
 
    public void centerButtons(int amount, int buttonLength) {
-      this.positionX = this.field_146294_l / 2 - buttonLength / 2;
+      this.positionX = this.width / 2 - buttonLength / 2;
       this.positionY = new Integer[amount];
-      int center = (this.field_146295_m + amount * 24) / 2;
+      int center = (this.height + amount * 24) / 2;
       int buttonStarts = center - amount * 24;
 
       for(int i = 0; i != amount; ++i) {
          this.positionY[i] = buttonStarts + 24 * i;
       }
-
    }
 
-   public void func_73863_a(int mouseX, int mouseY, float ticks) {
-      this.func_146276_q_();
-      super.func_73863_a(mouseX, mouseY, ticks);
-      super.func_73732_a(Minecraft.func_71410_x().field_71466_p, "Player nametags", this.field_146294_l / 2, this.positionY[2] + 7, 16777215);
+   public void drawScreen(int mouseX, int mouseY, float ticks) {
+      this.drawDefaultBackground();
+      super.drawScreen(mouseX, mouseY, ticks);
+      this.drawCenteredString(Minecraft.getMinecraft().fontRendererObj, "Player nametags", this.width / 2, this.positionY[2] + 7, 16777215);
       if (this.sliderOffset.dragging) {
          NametagRenderer.setOffset(this.sliderOffset.getValueInt());
       }
@@ -59,19 +58,19 @@ public class GUIMain extends GuiScreen {
       if (this.sliderScale.dragging) {
          NametagRenderer.setScale(this.sliderScale.getValueInt());
       }
-
    }
 
-   protected void func_146284_a(GuiButton button) throws IOException {
-      switch (button.field_146127_k) {
+   protected void actionPerformed(GuiButton button) throws IOException {
+      switch (button.id) {
          case 1:
             NametagRenderer.selftag = !NametagRenderer.selftag;
-            button.field_146126_j = "Own nametag: " + this.getSelfTag();
+            button.displayString = "Own nametag: " + this.getSelfTag();
+            break;
          default:
       }
    }
 
-   public void func_146281_b() {
+   public void onGuiClosed() {
       Main.config.get("Background", "Alpha", 25).set(this.alpha.getValueInt());
       Main.config.get("Player nametags", "Scale", 100).set(this.sliderScale.getValueInt());
       Main.config.get("Player nametags", "Y offset", 0).set(this.sliderOffset.getValueInt());
