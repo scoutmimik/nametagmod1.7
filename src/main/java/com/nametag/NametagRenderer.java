55f5b13ad03294dbe69f04de1de196dd81320844
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-10 19:09:27 +0200

    Update NametagRenderer.java

--- src/main/java/com/nametag/NametagRenderer.java
+++ src/main/java/com/nametag/NametagRenderer.java
@@ -37,48 +37,48 @@ public class NametagRenderer {
       y += (double)offset;
 
       if (canRenderName(entity)) {
-         EntityPlayerSP renderPlayer = Minecraft.func_71410_x().field_71439_g;
-         double d0 = entity.func_70068_e(renderPlayer);
-         float f = entity.func_70093_af() ? RendererLivingEntity.NAME_TAG_RANGE_SNEAK : RendererLivingEntity.NAME_TAG_RANGE;
+         EntityPlayerSP renderPlayer = Minecraft.getMinecraft().thePlayer;
+         double d0 = entity.getDistanceSqToEntity(renderPlayer);
+         float f = entity.isSneaking() ? RendererLivingEntity.NAME_TAG_RANGE_SNEAK : RendererLivingEntity.NAME_TAG_RANGE;
          if (d0 < (double)(f * f)) {
-            String s = entity.func_70005_c_();
+            String s = entity.getCommandSenderName();
             float f1 = 0.02666667F * scale;
             GL11.glAlphaFunc(516, 0.1F);
-            if (entity.func_70093_af()) {
-               FontRenderer fontrenderer = a.func_76983_a();
+            if (entity.isSneaking()) {
+               FontRenderer fontrenderer = a.getFontRendererFromRenderManager();
                GL11.glPushMatrix();
-               GL11.glTranslatef((float)x, (float)y + entity.field_70131_O + 0.5F - (entity.func_70631_g_() ? entity.field_70131_O / 2.0F : 0.0F), (float)z);
+               GL11.glTranslatef((float)x, (float)y + entity.height + 0.5F - (entity.isChild() ? entity.height / 2.0F : 0.0F), (float)z);
                GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-               GL11.glRotatef(-RenderManager.field_78727_a.field_78735_i, 0.0F, 1.0F, 0.0F);
-               GL11.glRotatef(RenderManager.field_78727_a.field_78732_j, 1.0F, 0.0F, 0.0F);
+               GL11.glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
+               GL11.glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
                GL11.glScalef(-f1, -f1, f1);
                GL11.glTranslatef(0.0F, 9.374999F, 0.0F);
                GL11.glDisable(2896);
                GL11.glDepthMask(false);
                GL11.glEnable(3042);
-               OpenGlHelper.func_148821_a(770, 771, 1, 0);
+               OpenGlHelper.glBlendFunc(770, 771, 1, 0);
                GL11.glDisable(3553);
-               int i = fontrenderer.func_78256_a(s) / 2;
-               Tessellator tessellator = Tessellator.field_78398_a;
-               tessellator.func_78382_b();
+               int i = fontrenderer.getStringWidth(s) / 2;
+               Tessellator tessellator = Tessellator.instance;
+               tessellator.startDrawingQuads();
                if (alpha != 0.0F) {
-                  tessellator.func_78369_a(0.0F, 0.0F, 0.0F, alpha);
-                  tessellator.func_78377_a((double)(-i - 1), (double)-1.0F, (double)0.0F);
-                  tessellator.func_78377_a((double)(-i - 1), (double)8.0F, (double)0.0F);
-                  tessellator.func_78377_a((double)(i + 1), (double)8.0F, (double)0.0F);
-                  tessellator.func_78377_a((double)(i + 1), (double)-1.0F, (double)0.0F);
+                  tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, alpha);
+                  tessellator.addVertex((double)(-i - 1), (double)-1.0F, (double)0.0F);
+                  tessellator.addVertex((double)(-i - 1), (double)8.0F, (double)0.0F);
+                  tessellator.addVertex((double)(i + 1), (double)8.0F, (double)0.0F);
+                  tessellator.addVertex((double)(i + 1), (double)-1.0F, (double)0.0F);
                }
 
-               tessellator.func_78381_a();
+               tessellator.draw();
                GL11.glEnable(3553);
                GL11.glDepthMask(true);
-               fontrenderer.func_78276_b(s, -fontrenderer.func_78256_a(s) / 2, 0, 553648127);
+               fontrenderer.drawString(s, -fontrenderer.getStringWidth(s) / 2, 0, 553648127);
                GL11.glEnable(2896);
                GL11.glDisable(3042);
                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
                GL11.glPopMatrix();
             } else {
-               playerRenderOffsetLivingLabel(entity, x, y - (entity.func_70631_g_() ? (double)(entity.field_70131_O / 2.0F) : (double)0.0F), z, s, 0.02666667F, d0);
+               playerRenderOffsetLivingLabel(entity, x, y - (entity.isChild() ? (double)(entity.height / 2.0F) : (double)0.0F), z, s, 0.02666667F, d0);
             }
          }
       }
@@ -88,12 +88,12 @@ public class NametagRenderer {
 
    protected static void playerRenderOffsetLivingLabel(EntityLivingBase entityIn, double x, double y, double z, String str, float p_177069_9_, double p_177069_10_) {
       if (p_177069_10_ < (double)100.0F) {
-         Scoreboard scoreboard = ((EntityPlayer)entityIn).func_96123_co();
+         Scoreboard scoreboard = ((EntityPlayer)entityIn).getWorldScoreboard();
          ScoreObjective scoreobjective = scoreboard.func_96539_a(2);
          if (scoreobjective != null) {
-            Score score = scoreboard.func_96529_a(entityIn.func_70005_c_(), scoreobjective);
-            renderLivingLabel(entityIn, score.func_96652_c() + " " + scoreobjective.func_96678_d(), x, y, z, 64, true);
-            y += (double)((float)a.func_76983_a().field_78288_b * 1.15F * p_177069_9_);
+            Score score = scoreboard.func_96529_a(entityIn.getCommandSenderName(), scoreobjective);
+            renderLivingLabel(entityIn, score.getScorePoints() + " " + scoreobjective.getDisplayName(), x, y, z, 64, true);
+            y += (double)((float)a.getFontRendererFromRenderManager().FONT_HEIGHT * 1.15F * p_177069_9_);
          }
       }
 
@@ -109,17 +109,17 @@ public class NametagRenderer {
          return false; // Ignoruje všetky entity okrem hráčov (vrátane mobov a NPC)
       }
 
-      EntityPlayerSP entityplayersp = Minecraft.func_71410_x().field_71439_g;
+      EntityPlayerSP entityplayersp = Minecraft.getMinecraft().thePlayer;
       if (entity == entityplayersp) {
          return selftag;
       } else {
-         Team team = entity.func_96124_cp();
-         Team team1 = entityplayersp.func_96124_cp();
+         Team team = entity.getTeam();
+         Team team1 = entityplayersp.getTeam();
          if (team != null && team1 != null) {
-            return team.func_142054_a(team1);
+            return team.isSameTeam(team1);
          }
 
-         return Minecraft.func_71382_s() && !entity.func_98034_c(entityplayersp) && entity.field_70153_n == null;
+         return Minecraft.isGuiEnabled() && !entity.isInvisibleToPlayer(entityplayersp) && entity.ridingEntity == null;
       }
    }
 
@@ -128,45 +128,45 @@ public class NametagRenderer {
    }
 
    protected static void renderLivingLabel(EntityLivingBase entityIn, String str, double x, double y, double z, int maxDistance, boolean isPlayer) {
-      EntityPlayerSP renderPlayer = Minecraft.func_71410_x().field_71439_g;
-      double d0 = entityIn.func_70068_e(renderPlayer);
+      EntityPlayerSP renderPlayer = Minecraft.getMinecraft().thePlayer;
+      double d0 = entityIn.getDistanceSqToEntity(renderPlayer);
       if (d0 <= (double)(maxDistance * maxDistance)) {
-         FontRenderer fontrenderer = a.func_76983_a();
+         FontRenderer fontrenderer = a.getFontRendererFromRenderManager();
          float f1 = isPlayer ? 0.02666667F * scale : 0.02666667F;
          GL11.glPushMatrix();
-         GL11.glTranslatef((float)x + 0.0F, (float)y + entityIn.field_70131_O + 0.5F, (float)z);
+         GL11.glTranslatef((float)x + 0.0F, (float)y + entityIn.height + 0.5F, (float)z);
          GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-         GL11.glRotatef(-RenderManager.field_78727_a.field_78735_i, 0.0F, 1.0F, 0.0F);
-         GL11.glRotatef(RenderManager.field_78727_a.field_78732_j, 1.0F, 0.0F, 0.0F);
+         GL11.glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
+         GL11.glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
          GL11.glScalef(-f1, -f1, f1);
          GL11.glDisable(2896);
          GL11.glDepthMask(false);
          GL11.glDisable(2929);
          GL11.glEnable(3042);
-         OpenGlHelper.func_148821_a(770, 771, 1, 0);
-         Tessellator tessellator = Tessellator.field_78398_a;
+         OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+         Tessellator tessellator = Tessellator.instance;
          int i = 0;
          if (str.equals("deadmau5")) {
             i = -10;
          }
 
-         int j = fontrenderer.func_78256_a(str) / 2;
+         int j = fontrenderer.getStringWidth(str) / 2;
          GL11.glDisable(3553);
-         tessellator.func_78382_b();
+         tessellator.startDrawingQuads();
          if (alpha != 0.0F) {
-            tessellator.func_78369_a(0.0F, 0.0F, 0.0F, alpha);
-            tessellator.func_78377_a((double)(-j - 1), (double)(-1 + i), (double)0.0F);
-            tessellator.func_78377_a((double)(-j - 1), (double)(8 + i), (double)0.0F);
-            tessellator.func_78377_a((double)(j + 1), (double)(8 + i), (double)0.0F);
-            tessellator.func_78377_a((double)(j + 1), (double)(-1 + i), (double)0.0F);
+            tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, alpha);
+            tessellator.addVertex((double)(-j - 1), (double)(-1 + i), (double)0.0F);
+            tessellator.addVertex((double)(-j - 1), (double)(8 + i), (double)0.0F);
+            tessellator.addVertex((double)(j + 1), (double)(8 + i), (double)0.0F);
+            tessellator.addVertex((double)(j + 1), (double)(-1 + i), (double)0.0F);
          }
 
-         tessellator.func_78381_a();
+         tessellator.draw();
          GL11.glEnable(3553);
-         fontrenderer.func_78276_b(str, -fontrenderer.func_78256_a(str) / 2, i, 553648127);
+         fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, i, 553648127);
          GL11.glEnable(2929);
          GL11.glDepthMask(true);
-         fontrenderer.func_78276_b(str, -fontrenderer.func_78256_a(str) / 2, i, -1);
+         fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, i, -1);
          GL11.glEnable(2896);
          GL11.glDisable(3042);
          GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
